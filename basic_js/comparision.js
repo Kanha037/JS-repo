a2c4ce5f500e50257af3244a94bb07fa1f89f021
Kahna Das <153742340+Kanha037@users.noreply.github.com>
2024-03-10 09:32:47 +0000

    compaeisions

---
+++ basic_js/comparision.js
@@ -0,0 +1,35 @@
+// Either true or false return value of comparision Statement
+// best comparision
+// console.log(2 > 2)
+// console.log(2 >= 2)
+// console.log(2 < 2)
+// console.log(2 <= 2)
+// console.log(2 == 2)
+
+//Confusion in change in Datatype
+//Automatic convert to number 
+// console.log("2">2)
+// console.log("02">=2)
+// console.log("002">=2)
+
+//In case of null comaparison
+ //>= and <= the result is true
+//  console.log(null >= 0)
+//  console.log(null <= 0)
+
+ //==,<,> the result is false
+//  console.log(null == 0)
+//  console.log(null < 0)
+//  console.log(null > 0)
+
+ // In case of undefined Comparision
+//All comparision are false output
+ console.log(undefined < 0)
+ console.log(undefined <= 0)
+ console.log(undefined >= 0)
+ console.log(undefined > 0)
+ console.log(undefined > 0)
+ console.log(undefined == 0)
+
+
+
