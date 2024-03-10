71c22bcd44e91d9d3988825b939fd5096faff248
Kahna Das <153742340+Kanha037@users.noreply.github.com>
2024-03-10 06:17:37 +0000

    variables definition

---
+++ basic_js/variable.js
@@ -0,0 +1,11 @@
+const accountId =144553
+let accountEmail="Kanha794@gmail.com"
+var accountPassword="12354"  //var is not used because of scope .
+accountCity="Dkl"
+let accountState
+// accountId=2
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
