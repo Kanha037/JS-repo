e9825deb9b1d62e4953fbdbd951d585a325c667f
Kahna Das <153742340+Kanha037@users.noreply.github.com>
2024-03-10 09:00:36 +0000

    operation in datatypes

--- basic_js/conversionofdatatype.js
+++ basic_js/conversionofdatatype.js
@@ -21,5 +21,29 @@ let booleanofloggedIn=Boolean(isloggedIn)
 
 let number =30
 let numTostr= String(number)
-console.log(numTostr);
-console.log(typeof numTostr);
+// console.log(numTostr);
+// console.log(typeof numTostr);
+
+// *************OPERATION*****************
+
+let value =3
+let negValue= -value;
+// console.log(negValue); 
+
+// console.log("Addition of 2+2 is "+(2+2)); 
+// console.log(2-2); 
+// console.log(2*2); 
+// console.log(2/2); 
+// console.log(2%2); 
+// console.log("Power of 2 to 3 is "+(2**3))
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter=100
+++gameCounter
+console.log(++gameCounter);//Pre Increment 101+1=102 gameCounter=102
+console.log(gameCounter++);//gameCounter=102
+console.log(gameCounter)//gameCounter=103 Post Increament 102 to 103
+
