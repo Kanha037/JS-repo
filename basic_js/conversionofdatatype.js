6ffeaebd7e315b8aca16dad88177ecc7ee056220
Kahna Das <153742340+Kanha037@users.noreply.github.com>
2024-03-10 08:36:06 +0000

    conversion of datatype

---
+++ basic_js/conversionofdatatype.js
@@ -0,0 +1,25 @@
+let age=true
+// console.log(typeof age)
+
+let valueInNumber=Number(age)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
+
+//"33" => 33
+//"33a" => NaN (Not a Number)but type is number.
+//true => 1 false=>0
+
+let isloggedIn="Kanha"
+
+let booleanofloggedIn=Boolean(isloggedIn)
+// console.log(booleanofloggedIn);
+
+//1 => true
+//0 => false
+//"" => false
+//"kanha" => true
+
+let number =30
+let numTostr= String(number)
+console.log(numTostr);
+console.log(typeof numTostr);
