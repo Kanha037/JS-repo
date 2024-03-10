81e6dda030e22b0f934aa510fa5c4b7e3efaa0f1
Kahna Das <153742340+Kanha037@users.noreply.github.com>
2024-03-10 07:00:34 +0000

    datatype

---
+++ basic_js/datatypes.js
@@ -0,0 +1,17 @@
+"use strict";//treat as new version JS code.
+//alert() is used in browser.
+console.log(3+3)
+console.log("kanha") 
+let age=18
+let isloggedin=false;
+let state=null;
+//number => 2 to 53
+//bigint
+//string
+//Boolean
+// null
+//undefined =>
+//Symbol
+
+// Object
+console.log(typeof undefined)
